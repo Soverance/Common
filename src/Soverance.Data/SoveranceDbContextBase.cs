fd3030418f6f341eddec267fba4e6f2d648801f3
Scott McCutchen <scott.mccutchen@soverance.com>
2026-03-28 21:58:12 -0400

    session persistence

--- src/Soverance.Data/SoveranceDbContextBase.cs
+++ src/Soverance.Data/SoveranceDbContextBase.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Soverance.Auth.Models;
 
 namespace Soverance.Data;
 
-public abstract class SoveranceDbContextBase : DbContext
+public abstract class SoveranceDbContextBase : DbContext, IDataProtectionKeyContext
 {
     protected SoveranceDbContextBase(DbContextOptions options) : base(options) { }
 
@@ -11,6 +12,7 @@ public abstract class SoveranceDbContextBase : DbContext
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
     public DbSet<SamlConfig> SamlConfigs => Set<SamlConfig>();
     public DbSet<SamlRoleMapping> SamlRoleMappings => Set<SamlRoleMapping>();
+    public DbSet<DataProtectionKey> DataProtectionKeys => Set<DataProtectionKey>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
